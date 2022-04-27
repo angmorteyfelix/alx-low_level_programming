aebcfd6e4d9fb2656eae451a45d8b9b819bdefa7
_Felixz_ <angmorteyfelix40@gmail.com>
2022-04-27 12:52:08 +0000

    Task 101

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,8 +2,10 @@
 
 /**
  * infinite_add - adds two numbers
- * @n1: first number, * @n2: second number
- * @r: buffer for result, * @size_r: buffer size
+ * @n1: first number,
+ * @n2: second number
+ * @r: buffer for result,
+ * @size_r: buffer size
  * Return: address of r or 0
  */
 
@@ -11,10 +13,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
+	for (i = 0; n1[i]; i++);
+
+	for (j = 0; n2[j]; j++);
+
 	if (i > size_r || j > size_r)
 		return (0);
 	m = 0;
