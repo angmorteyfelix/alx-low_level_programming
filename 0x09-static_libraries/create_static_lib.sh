0566a18140f5762442a39e14842fce7d8bad89f5
_Felixz_ <angmorteyfelix40@gmail.com>
2022-05-04 13:57:14 +0000

    Argc_Argv

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                                                        
-gcc -Wall -pedantic -Werror -Wextra -c *.c ar -rc lib:all.a *.o ranlib liball.a
+gcc -Wall -c *.c ar rc liball.a *.o ranlib liball.a
 
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Readme for argc_argv
