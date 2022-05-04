0eb72bd842911e5c669eb05561a692de74d8461e
_Felixz_ <angmorteyfelix40@gmail.com>
2022-05-04 13:01:54 +0000

    Task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                        
 gcc -Wall -pedantic -Werror -Wextra -c *.c ar -rc lib:all.a *.o ranlib liball.a
 
